c3bfc68b66567267c1664436ceeab6061af1eb82
Yaroslav Fedosenko <fedosyaros@gmail.com>
2023-06-30 01:19:16 +0300

    30.06.2023

--- js/main.js
+++ js/main.js
@@ -10,8 +10,6 @@ const ADVT_MARKERS_COUNT = 10;
 
 setUserFormSubmit(showSuccessMessage);
 getData((offers) => {
-  // createPins(offers.slice(0, ADVT_MARKERS_COUNT));
-  // console.log(offers);
   offers.slice(0, ADVT_MARKERS_COUNT).forEach(offer => {
     createPins(offer);
   });
